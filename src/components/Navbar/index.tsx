3150859cdbd5b172e3f095d3e01211cdbd7363a4
Sharma <amansharma@Sharmas-MacBook-Pro.local>
2023-07-09 15:34:13 +0530

    Navbar Changes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,89 +1,90 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {FaInstagram , FaFacebook} from 'react-icons/fa'
+import {FaBars,FaTimes} from "react-icons/fa"
+
 import { ButtonSecondary } from '..'
+import { navLinks } from '@/constants'
+import menu from '../../../public/menu.svg'
 
 type Props = {}
 
 const index = (props: Props) => {
+  let Links =[
+    {name:"The App",link:"/"},
+    {name:"What's Poppin?",link:"/"},
+    {name:"Ambassadors",link:"/"},
+    {name:"Blog's",link:"/"}
+    
+  ];
+  const [scroll,setScroll] = useState(false);
+  
+  useEffect(()=>{
+    if(window.scrollY >=80){
+      setScroll(true);
+    } else{
+        setScroll(false);
+    }
+    console.log("Scroll",scroll)
+  },[])
+
+  
+  let [open,setOpen]=useState(false);
+
   return (
-    <div className='max-w-full flex justify-center'>
-     <header className="bg-background max-w-screen-lg w-full	">
-        <nav className='flex w-full justify-between	 max-h-16	 p-4'>
-          <div className="hover:border-b-4 hover:border-b-white text-xl">
-          <Link
-                href="/"
-                className="border-none text-white"
-              >
-                Blend
-              </Link>
-          </div>
-          <div>
-          <ul className="flex  flex-wrap text-xl">
-            <li className="mr-6 hover:border-b-4 hover:border-b-white">
-              <Link
-                href="/"
-                className="border-none text-white"
-              >
-                The app
-              </Link>
-            </li>
-            <li className="mr-6 hover:border-b-4 hover:border-b-white">
-              <Link
-                href="/about/"
-                className="border-none text-white"
-              >
-                What's Poppin?
-              </Link>
-            </li>
-            <li className="mr-6 hover:border-b-4 hover:border-b-white">
-              <a
-                className="text-white"
-                href="https://github.com/ixartz/Next-js-Boilerplate"
-              >
-                Tastemakers
-              </a>
-            </li>
-            <li className="mr-6 hover:border-b-4 hover:border-b-white ">
+    <nav className={`shadow-md  w-full fixed top-0 left-0 z-[999]`}>
+      <div className={`md:flex max-w-7xl mx-auto items-center justify-between ${open?'bg-white':'bg-none'} py-4 md:px-10 px-7 transition-all duration-500 ease-in `}>
+        <div className='font-bold text-2xl cursor-pointer flex items-center
+        text-gray-800'>
+          <span className='text-3xl text-white font-semibold mr-1 pt-2'>
+          
+          Blend
+          </span>
+        
+        </div>
+    
+    <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+     {/* <ion-icon name={open ? 'close':'menu'}></ion-icon> */}
+     {open ? <div className='text-primary'><FaTimes/></div>:<div className='text-white'><FaBars/></div>}
+    </div>
+
+    <ul className={` md:flex md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-50 left-0 w-full justify-end md:pl-0  transition-all duration-500 ease-in ${open ? 'top-20 bg-white':'top-[-490px] '}`}>
+      {
+        Links.map((link)=>(
+          <li key={link.name} style={{textDecoration:'none'}} className='md:ml-8 text-xl md:my-0 my-7 '>
+            <Link style={{textDecoration:'none'}}  href={link.link} className={`${open?'text-gray-800 text-center':'text-white'}  cursor-pointer hover:text-[#96f]  duration-500`}><h1>{link.name}</h1></Link>
+          </li>
+        ))
+      }
+        <div className={`flex flex-row ${open?'justify-evenly px-32':''} `}>
+          <li className="md:ml-8 text-4xl md:my-0 my-7 ">
               <Link
                 href="/blog/"
-                className="no-underline text-white"
-              >
-                Blog
-              </Link>
-            </li>
-            <li className="mr-6 hover:border-b-4 hover:border-b-white ">
-              <Link
-                href="/blog/"
-                className="no-underline text-white text-3xl"
+                className={`${open?'text-gray-800':'text-white'} cursor-pointer hover:text-[#96f]  duration-500`}
               >
                 <FaInstagram/>
               </Link>
-            </li>
-            <li className="mr-6 hover:border-b-4 hover:border-b-white ">
+          </li>
+          <li className="md:ml-8 md:my-0 my-7 text-4xl mr-4">
               <Link
                 href="/blog/"
-                className="no-underline text-white text-3xl"
+                className={`${open?'text-gray-800 text-center':'text-white'}  cursor-pointer hover:text-[#96f]  duration-500`}
                >
                   <FaFacebook/>
               </Link>
-            </li>
-            <div>
-               <ButtonSecondary 
+           </li>
+           </div>
+              <ButtonSecondary 
                marginBottom='mb-0'
                marginTop='mt-0'
                text="Join The Club"
-               textColor='text-[#9966ff]'
-               background='bg-primaryWhite'
-               minWidth='min-w-[150px]'
+               textColor={`${open?'text-white':'text-[#9966ff]'}`}
+               background={`${open?'bg-primary':'bg-primaryWhite'}`}
+               minWidth={`${open?'max-w-[280px] ml-12 ':'max-w-[150px]'}`}
                ></ButtonSecondary>
-            </div>
-          </ul>
-         
-          </div>
-        </nav>
-      </header>
+      </ul>
     </div>
+  </nav>
   )
 }
 
--- src/sections/HeroSection/index.tsx
+++ src/sections/HeroSection/index.tsx
@@ -19,7 +19,7 @@ const index = (props: Props) => {
     flex 
     flex-col
     justify-center 
-    items-center pt-[18vh] mt-[-75px] pb-5'>
+    items-center pt-[18vh] md:mt-20 pb-5'>
       <div className='infoHolder max-w-7xl mb-7'>
         <Intro />        
       </div>     
