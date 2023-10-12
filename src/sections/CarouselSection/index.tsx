991252a560259b530c9c8c9df619c4df5a8806ee
TanishqColPal <bp_tanishq_sharma@colpal.com>
2023-10-13 01:51:50 +0530

    added carousel

--- src/sections/CarouselSection/index.tsx
+++ src/sections/CarouselSection/index.tsx
@@ -44,7 +44,7 @@ const index = (props: Props) => {
        dotsClass= 'owl-dots'
        
        >
-        <div className='flex h-[55vh] w-full justify-center items-center '>
+        <div className='flex h-[75vh] w-full justify-center items-center '>
           <div className={`${styles.msContent}   text-left px-6 pt-10 lg:px-0 lg:pt-0`}>
               <h1 className='text-[2.5rem] lg:text-[3.5rem]  leading-tight	font-bold	mb-4	'>Match based on
 music you stream</h1>
